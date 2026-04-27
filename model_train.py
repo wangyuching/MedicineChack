905d62f71181df391019ac59ce0a85dd04f81bce
wang <5b1g0023@stuts.edu.tw>
2026-04-27 18:26:22 +0800

    success epochs=2,     batch=16,     imgsz=640,     device=0,

--- model_train.py
+++ model_train.py
@@ -4,10 +4,11 @@ if __name__ == "__main__":
     model = YOLO("yolo26n-obb.pt")
     results = model.train(
     data="./data.yaml",
-    epochs=2,
+    epochs=100,
     batch=16,
     imgsz=640,
     device=0,
+    workers=0,
     project="Yolo26n_OBB_Train",
-    name="d0427_v1_e2_b16_i640"
+    name="d0427_v1_e100_b16_i640"
     )
