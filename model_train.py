d1877b44ff93aec461d2c4a240a8f190d7cf6dca
wang <5b1g0023@stuts.edu.tw>
2026-04-27 21:26:41 +0800

    v1 train done
        epochs=100,
        batch=16,
        imgsz=640,
        device=0,
        workers=0,
        project="Yolo26s_OBB_Train",
        name="d0427_v1_e100_b16_i640"

--- model_train.py
+++ model_train.py
@@ -1,7 +1,7 @@
 from  ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("yolo26n-obb.pt")
+    model = YOLO("yolo26s-obb.pt")
     results = model.train(
     data="./data.yaml",
     epochs=100,
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     imgsz=640,
     device=0,
     workers=0,
-    project="Yolo26n_OBB_Train",
+    project="Yolo26s_OBB_Train",
     name="d0427_v1_e100_b16_i640"
     )
