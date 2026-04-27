42fb6c244f62c8287a7f4d8a0f43c9ffdd025b5c
wang <5b1g0023@stuts.edu.tw>
2026-04-27 16:57:22 +0800

    1656

---
+++ addFile/createDataYaml.py
@@ -0,0 +1,43 @@
+# Python function to automatically create data.yaml config file
+# 1. Reads "classes.txt" file to get list of class names
+# 2. Creates data dictionary with correct paths to folders, number of classes, and names of classes
+# 3. Writes data in YAML format to data.yaml
+
+from torch import cat
+import yaml
+import os
+
+def create_data_yaml(path_to_classes_txt, path_to_data_yaml):
+
+  # Read class.txt to get class names
+  if not os.path.exists(path_to_classes_txt):
+    print(f'classes.txt file not found! Please create a classes.txt labelmap and move it to {path_to_classes_txt}')
+    return
+  with open(path_to_classes_txt, 'r') as f:
+    classes = []
+    for line in f.readlines():
+      if len(line.strip()) == 0: continue
+      classes.append(line.strip())
+  number_of_classes = len(classes)
+
+  # Create data dictionary
+  data = {
+      'path': './data',
+      'train': 'train/images',
+      'val': 'validation/images',
+      'nc': number_of_classes,
+      'names': classes
+  }
+
+  # Write data to YAML file
+  with open(path_to_data_yaml, 'w') as f:
+    yaml.dump(data, f, sort_keys=False)
+  print(f'Created config file at {path_to_data_yaml}')
+
+  return
+
+# Define path to classes.txt and run function
+path_to_classes_txt = './data/classes.txt'
+path_to_data_yaml = './data.yaml'
+
+create_data_yaml(path_to_classes_txt, path_to_data_yaml)
---
+++ model_train.py
@@ -0,0 +1,12 @@
+from  ultralytics import YOLO
+
+model = YOLO("yolo26n-obb.pt")
+results = model.train(
+    data="./data.yaml",
+    epochs=100,
+    batch=16,
+    imgsz=640,
+    device=0,
+    project="Yolo26n_OBB_Train",
+    name="d0427_v1_e100_b16_i640"
+)
