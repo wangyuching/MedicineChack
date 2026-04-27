b18fc00f5b59ddb60f73e203fd21f7ef26ddcf6b
wang <5b1g0023@stuts.edu.tw>
2026-04-27 17:40:23 +0800

    must in terminal do python filename.py

--- model_train.py
+++ model_train.py
@@ -4,9 +4,9 @@ model = YOLO("yolo26n-obb.pt")
 results = model.train(
     data="./data.yaml",
     epochs=100,
-    batch=16,
+    batch=8,
     imgsz=640,
     device=0,
     project="Yolo26n_OBB_Train",
-    name="d0427_v1_e100_b16_i640"
+    name="d0427_v1_e100_b8_i640"
 )
