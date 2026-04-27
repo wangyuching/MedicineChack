61ce11666234a92ae734b5a3db6834429ab3ced3
wang <5b1g0023@stuts.edu.tw>
2026-04-27 17:52:00 +0800

    test epoch=2

--- model_train.py
+++ model_train.py
@@ -1,12 +1,13 @@
 from  ultralytics import YOLO
 
-model = YOLO("yolo26n-obb.pt")
-results = model.train(
+if __name__ == "__main__":
+    model = YOLO("yolo26n-obb.pt")
+    results = model.train(
     data="./data.yaml",
-    epochs=100,
+    epochs=2,
     batch=8,
     imgsz=640,
     device=0,
     project="Yolo26n_OBB_Train",
     name="d0427_v1_e100_b8_i640"
-)
+    )
