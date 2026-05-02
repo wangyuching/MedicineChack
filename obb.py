12faf9f54ed0137487a71ed8291b13079be95936
wang <5b1g0023@stuts.edu.tw>
2026-05-02 11:28:24 +0800

    obb.pt test success

---
+++ obb.py
@@ -0,0 +1,20 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("my_model.pt")
+
+cap = cv2.VideoCapture(1)
+
+while cap.isOpened():
+    ok, frame = cap.read()
+    if ok:
+        results = model(frame)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLO26 OBB Streaming", annotated_frame)
+    
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
