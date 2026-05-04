fe6a34215c8beeae747e9c24032d39ae518e0b1a
wang <5b1g0023@stuts.edu.tw>
2026-05-04 16:49:52 +0800

    橫向的成功分隔，直向持續嘗試

--- test_img.py
+++ test_img.py
@@ -25,10 +25,30 @@ for r in results:
         print(f"There's no objects for Class {target_cls}")
 
     for i, box in enumerate(filter_boxes):
-        points = box.numpy().astype(np.int32)
-        print(points, end="\n\n")
-        cv2.polylines(img, [points], isClosed=True, color=(0, 0, 255), thickness=2)
+        points = box.numpy().astype(np.float32)
+
+        width = int(np.linalg.norm(points[1] - points[2]))
+        height = int(np.linalg.norm(points[1] - points[0]))
+
+        disten_points = np.array([
+            [width, height], [width, 0], [0, 0], [0, height]
+            ], dtype=np.float32)
+
+        matrix = cv2.getPerspectiveTransform(points, disten_points)
+        cropped_img = cv2.warpPerspective(img, matrix, (width, height))
+        w_step = width // 4
+        sub_parts = []
+        for i in range(4):
+            sub_part = cropped_img[:, i*w_step : (i+1)*w_step]
+            sub_parts.append(sub_part)
+
+            cv2.imshow(f"Part{i}", sub_part)
 
 cv2.imshow("poly", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+    # for i, box in enumerate(filter_boxes):
+    #     points = box.numpy().astype(np.int32)
+    #     print(points, end="\n\n")
+    #     cv2.polylines(img, [points], isClosed=True, color=(0, 0, 255), thickness=2)
