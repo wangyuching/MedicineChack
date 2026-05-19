518522818d169ea07d21a5d9a64e2fa97bb53fad
wang <5b1g0023@stuts.edu.tw>
2026-05-19 16:08:55 +0800

    text can show on html

--- alotdef.py
+++ alotdef.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import time as t
+from db import db, Pill
 
 def get_target_obb(results, target_cls):
     filtered_boxes = []
@@ -156,3 +158,80 @@ def draw_slot_states(image, box, slot_idx, slot_data):
     if lid_state == "Open":
         pill_label = f"{pill_state}"
         cv2.putText(image, pill_label, (int(x) - 30, int(y) + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+def insert_pill_data(current_slots_data, frame):
+    dt_str = t.strftime("%Y-%m-%d %H:%M:%S", t.localtime())
+
+    lids = [current_slots_data[i]['lid'] for i in range(4)]
+    has_pills = [
+        "Unknown" if current_slots_data[i]['lid'] == "Close" else
+        ("Full" if current_slots_data[i]['Has_pill'] else "Empty")
+        for i in range(4)
+    ]
+    _, buffer = cv2.imencode('.jpg', frame)
+    img_data = buffer.tobytes()
+
+    try:
+        new_data = Pill(
+            dt=dt_str,
+            lid0=lids[0],
+            lid1=lids[1],
+            lid2=lids[2],
+            lid3=lids[3],
+            has_pill0=has_pills[0],
+            has_pill1=has_pills[1],
+            has_pill2=has_pills[2],
+            has_pill3=has_pills[3],
+            img=img_data
+        )
+        db.session.add(new_data)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error inserting data: {e}")
+    
+
+def save_frame(frame, current_slots_data, tracker, duration, missing):
+    current_opens = [
+        idx for idx, 
+        data in current_slots_data.items() 
+        if data['lid'] == "Open"
+    ]
+
+    if len(current_opens) > 0:
+        tracker['missing_start_time'] = None
+
+        if current_opens != tracker['active_opens']:
+            tracker['active_opens'] = current_opens
+            tracker['open_start_time'] = t.time()
+            tracker['triggered'] = False
+
+        else:
+            if tracker['open_start_time'] is not None and not tracker['triggered']:
+                elapsed_time = t.time() - tracker['open_start_time']
+
+                if elapsed_time > duration:
+                    slot_details = []#will delete at fininal
+                    for idx in current_opens:#will delete at fininal
+                        pill_state = "Full" if current_slots_data[idx]['Has_pill'] else "Empty"#will delete at fininal
+                        slot_details.append(f"slot{idx}_{pill_state}")#will delete at fininal
+                    slots_str = "_".join(slot_details)#will delete at fininal
+                    timestamp = t.strftime("%Y%m%d_%H%M%S")#will delete at fininal
+                    filename = f"saved_slots/{timestamp}_{slots_str}.png"#will delete at fininal
+                    cv2.imwrite(filename, frame)#will delete at fininal
+
+                    insert_pill_data(current_slots_data, frame)
+                    tracker['triggered'] = True
+    
+    else:
+        if tracker['open_start_time'] is not None:
+            if tracker['missing_start_time'] is None:
+                tracker['missing_start_time'] = t.time()
+
+            lost_duration = t.time() - tracker['missing_start_time']
+
+            if lost_duration > missing:
+                tracker['active_opens'] = []
+                tracker['open_start_time'] = None
+                tracker['missing_start_time'] = None
+                tracker['triggered'] = False
--- app.py
+++ app.py
@@ -6,16 +6,25 @@ import numpy as np
 from flask import Flask, render_template, Response, jsonify
 import base64
 
-from alotdef import (get_target_obb, 
-                     draw_target_obb, 
-                     split_obb, 
-                     lid_connect_split_box, 
-                     pillbox_head_tail, 
-                     check_pill_in_split_box,
-                     draw_slot_states)
-from db import Pill, save_frame
+from alotdef import (
+    get_target_obb, 
+    split_obb, 
+    lid_connect_split_box, 
+    pillbox_head_tail, 
+    check_pill_in_split_box,
+    draw_slot_states,
+    save_frame
+    )
+from db import db, Pill
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:DataBase@127.0.0.1:3306/project"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 model = YOLO("best.pt", task="obb") #best.float32.tflite, best.onnx
 cap = cv2.VideoCapture(1)
@@ -38,92 +47,114 @@ same_time_tracker = {
 }
 
 def cap_real_time():
-    while cap.isOpened():
-        ok, frame = cap.read()
-        if (not ok) | (frame is None):    
-            print("usb pull out and in again...")
-            break
-        else:
-            frame = cv2.resize(frame, (640, 480))
-
-            results = model(frame, verbose=False)
-            annotated_frame = results[0].plot()
-            annotated_frame = cv2.resize(annotated_frame, (640, 480))
-
-            pill_detect_frame = frame.copy()
-            pill_detect_frame = cv2.resize(pill_detect_frame, (640, 480))
-
-            bedtime_word = get_target_obb(results, target_cls=0)
-            pill_boxes = get_target_obb(results, target_cls=4)
-            if pill_boxes:
-                lid_close = get_target_obb(results, target_cls=1)
-                lid_open = get_target_obb(results, target_cls=3)
-                all_lids = []
-                for ls in lid_close: all_lids.append({'box': ls, 'state': 'Close'})
-                for lo in lid_open: all_lids.append({'box': lo, 'state': 'Open'})
-
-                for pb in pill_boxes:
-                    if bedtime_word:
-                        current_res = pillbox_head_tail(bedtime_word, pb)
-                        reverse_state = current_res
-                        has_once_detect_bedtime_word = True
-                        print("Direction updated by bedtime_word.")
-                    elif has_once_detect_bedtime_word:
-                        pass
-                        print("Direction kept from last detection.")
-
-                    w, h = pb[2], pb[3]
-                    split_axis = "w" if w > h else "h"
-                    sub_boxes = split_obb(pb, split_axis, num_splits=4, reverse=reverse_state)
-
-                    slots_data = {i: {"lid" : "Missing", "Has_pill": False} for i in range(4)}
-                    for lid in all_lids:
-                        idx = lid_connect_split_box(lid['box'], sub_boxes)
-                        if idx != -1:
-                            slots_data[idx]['lid'] = lid['state']
-
-                    for i, sub_box in enumerate(sub_boxes):
-                        current_lid_state = slots_data[i]['lid']
-                        if current_lid_state == "Open":
-                            has_pill, mask = check_pill_in_split_box(pill_detect_frame, i, sub_box, HSV_LOWER, HSV_UPPER)
-                            slots_data[i]['Has_pill'] = has_pill
-                        else:
-                            slots_data[i]['Has_pill'] = False
-                        
-                        if i == 3:
-                            cv2.putText(pill_detect_frame, "TAIL", (int(sub_box[0]), int(sub_box[1]-20)), 
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
-                        
-                        draw_slot_states(pill_detect_frame, sub_box, i, slots_data[i])
-
-                    save_frame(
-                        frame=pill_detect_frame,
-                        current_slots_data=slots_data,
-                        tracker=same_time_tracker,
-                        duration=5.0,
-                        missing=5.0
-                    )
-
+    with app.app_context():
+        while cap.isOpened():
+            ok, frame = cap.read()
+            if (not ok) | (frame is None):    
+                print("usb pull out and in again...")
+                break
             else:
-                print("Cant find object pill_box.")
+                frame = cv2.resize(frame, (640, 480))
 
-            combined_frame = np.hstack((frame, annotated_frame, pill_detect_frame))
-            final_view = cv2.resize(combined_frame, (0, 0), fx=0.7, fy=0.7)
+                results = model(frame, verbose=False)
+                annotated_frame = results[0].plot()
+                annotated_frame = cv2.resize(annotated_frame, (640, 480))
 
-            ret, jpeg = cv2.imencode('.jpg', pill_detect_frame)
-            pill_detect_frame = jpeg.tobytes()
-            yield(
-                b'--pill_detect_frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + pill_detect_frame + b'\r\n'
-            )
+                pill_detect_frame = frame.copy()
+                pill_detect_frame = cv2.resize(pill_detect_frame, (640, 480))
 
+                bedtime_word = get_target_obb(results, target_cls=0)
+                pill_boxes = get_target_obb(results, target_cls=4)
+                if pill_boxes:
+                    lid_close = get_target_obb(results, target_cls=1)
+                    lid_open = get_target_obb(results, target_cls=3)
+                    all_lids = []
+                    for ls in lid_close: all_lids.append({'box': ls, 'state': 'Close'})
+                    for lo in lid_open: all_lids.append({'box': lo, 'state': 'Open'})
+
+                    for pb in pill_boxes:
+                        if bedtime_word:
+                            current_res = pillbox_head_tail(bedtime_word, pb)
+                            reverse_state = current_res
+                            has_once_detect_bedtime_word = True
+                            print("Direction updated by bedtime_word.")
+                        elif has_once_detect_bedtime_word:
+                            pass
+                            print("Direction kept from last detection.")
+
+                        w, h = pb[2], pb[3]
+                        split_axis = "w" if w > h else "h"
+                        sub_boxes = split_obb(pb, split_axis, num_splits=4, reverse=reverse_state)
+
+                        slots_data = {i: {"lid" : "Missing", "Has_pill": False} for i in range(4)}
+                        for lid in all_lids:
+                            idx = lid_connect_split_box(lid['box'], sub_boxes)
+                            if idx != -1:
+                                slots_data[idx]['lid'] = lid['state']
+
+                        for i, sub_box in enumerate(sub_boxes):
+                            current_lid_state = slots_data[i]['lid']
+                            if current_lid_state == "Open":
+                                has_pill, mask = check_pill_in_split_box(pill_detect_frame, i, sub_box, HSV_LOWER, HSV_UPPER)
+                                slots_data[i]['Has_pill'] = has_pill
+                            else:
+                                slots_data[i]['Has_pill'] = False
+                            
+                            if i == 3:
+                                cv2.putText(pill_detect_frame, "TAIL", (int(sub_box[0]), int(sub_box[1]-20)), 
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+                            
+                            draw_slot_states(pill_detect_frame, sub_box, i, slots_data[i])
+
+                        save_frame(
+                            frame=pill_detect_frame,
+                            current_slots_data=slots_data,
+                            tracker=same_time_tracker,
+                            duration=5.0,
+                            missing=5.0
+                        )
+
+                else:
+                    print("Cant find object pill_box.")
+
+                combined_frame = np.hstack((frame, annotated_frame, pill_detect_frame))
+                final_view = cv2.resize(combined_frame, (0, 0), fx=0.7, fy=0.7)
+
+                ret, jpeg = cv2.imencode('.jpg', pill_detect_frame)
+                pill_detect_frame = jpeg.tobytes()
+                yield(
+                    b'--pill_detect_frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + pill_detect_frame + b'\r\n'
+                )
     cap.release()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/api/history')
+def api_history():
+    history = Pill.query.all()
+    history_list = []
+    for record in history:
+        def to_base64(img_blob):
+            if img_blob:
+                return base64.b64encode(img_blob).decode('utf-8')
+            return None
 
+        history_list.append({
+            'dt': record.dt.strftime("%Y-%m-%d %H:%M:%S"),
+            'lid0': record.lid0,
+            'lid1': record.lid1,
+            'lid2': record.lid2,
+            'lid3': record.lid3,
+            'has_pill0': record.has_pill0,
+            'has_pill1': record.has_pill1,
+            'has_pill2': record.has_pill2,
+            'has_pill3': record.has_pill3,
+            'img': to_base64(record.img)
+        })
+    return jsonify(history_list)
 
 @app.route('/cap_in_html')
 def cap_in_html():
